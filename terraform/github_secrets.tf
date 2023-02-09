f4c423f4c618f88bbc3d6824c95c507b8231188e
Colin Tessarzick <colintessarzick@hotmail.com>
2023-02-09 18:13:13 +0100

    fix: adjust gh repo name ref

--- terraform/github_secrets.tf
+++ terraform/github_secrets.tf
@@ -1,9 +1,9 @@
-data "github_repository" "this" {
-  name = var.deployment_name
-}
+# data "github_repository" "this" {
+#   name = var.deployment_name
+# }
 
 resource "github_actions_secret" "app_runner_id" {
-  repository      = data.github_repository.this.name
+  repository      = var.deployment_name
   secret_name     = "APP_RUNNER_ID"
   plaintext_value = aws_apprunner_service.api_service.id
 }
