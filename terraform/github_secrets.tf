15c4e769da04edf6dbb82f287fec84bd1c55687e
Colin Tessarzick <colintessarzick@hotmail.com>
2023-02-09 19:24:46 +0100

    feat: add gh actions secret for App Runner ARN

--- terraform/github_secrets.tf
+++ terraform/github_secrets.tf
@@ -1,9 +1,11 @@
-# data "github_repository" "this" {
-#   name = var.deployment_name
-# }
-
 resource "github_actions_secret" "app_runner_id" {
   repository      = var.deployment_name
   secret_name     = "APP_RUNNER_ID"
   plaintext_value = aws_apprunner_service.api_service.id
+}
+
+resource "github_actions_secret" "app_runner_role_arn" {
+  repository      = var.deployment_name
+  secret_name     = "APP_RUNNER_ROLE_ARN"
+  plaintext_value = aws_iam_role.api_service_app_runner.arn
 }
