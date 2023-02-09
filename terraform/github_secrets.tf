be6110575adc53d0f7271e508f74314db29af22e
Colin Tessarzick <colintessarzick@hotmail.com>
2023-02-09 20:35:49 +0100

    refactor: use aws cli for deployment

--- terraform/github_secrets.tf
+++ terraform/github_secrets.tf
@@ -6,6 +6,6 @@ resource "github_actions_secret" "app_runner_id" {
 
 resource "github_actions_secret" "app_runner_role_arn" {
   repository      = var.deployment_name
-  secret_name     = "APP_RUNNER_ROLE_ARN"
-  plaintext_value = aws_iam_role.api_service_app_runner.arn
+  secret_name     = "APP_RUNNER_ARN"
+  plaintext_value = aws_apprunner_service.api_service.service_arn
 }
