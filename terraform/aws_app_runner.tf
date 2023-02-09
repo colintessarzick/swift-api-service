e46a9bc445e8a383cecf37305f7817225b109475
Colin Tessarzick <colintessarzick@hotmail.com>
2023-02-09 16:49:40 +0100

    feat: add tf config for app runner

--- terraform/aws_app_runner.tf
+++ terraform/aws_app_runner.tf
@@ -1,20 +1,20 @@
-# resource "aws_apprunner_service" "api_service" {
-#   provider = aws.ireland
+resource "aws_apprunner_service" "api_service" {
+  provider = aws.ireland
 
-#   service_name = local.resource_prefix
+  service_name = local.resource_prefix
 
-#   source_configuration {
-#     image_repository {
-#       image_configuration {
-#         port = "5813"
-#       }
-#       image_identifier      = "${aws_ecr_repository.api_service.repository_url}:latest"
-#       image_repository_type = "ECR"
-#     }
-#     auto_deployments_enabled = false
+  source_configuration {
+    image_repository {
+      image_configuration {
+        port = "5813"
+      }
+      image_identifier      = "${aws_ecr_repository.api_service.repository_url}:latest"
+      image_repository_type = "ECR"
+    }
+    auto_deployments_enabled = false
 
-#     authentication_configuration {
-#       access_role_arn = aws_iam_role.api_service_app_runner.arn
-#     }
-#   }
-# }
+    authentication_configuration {
+      access_role_arn = aws_iam_role.api_service_app_runner.arn
+    }
+  }
+}
