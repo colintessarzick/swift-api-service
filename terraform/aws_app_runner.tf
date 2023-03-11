8c4c1cbe0c2d3d2133dd7c71f50e1a3ff53ee833
Colin Tessarzick <colintessarzick@hotmail.com>
2023-03-11 21:50:56 +0100

    fix: explicitly add depending resources

--- terraform/aws_app_runner.tf
+++ terraform/aws_app_runner.tf
@@ -22,6 +22,8 @@ resource "aws_apprunner_service" "api_service" {
 }
 
 resource "aws_apprunner_auto_scaling_configuration_version" "api_service" {
+  provider = aws.ireland
+
   auto_scaling_configuration_name = local.resource_prefix
 
   max_concurrency = 30
@@ -30,6 +32,8 @@ resource "aws_apprunner_auto_scaling_configuration_version" "api_service" {
 }
 
 resource "aws_apprunner_custom_domain_association" "api_service" {
+  provider = aws.ireland
+
   domain_name = local.public_service_domain
   service_arn = aws_apprunner_service.api_service.arn
 }
