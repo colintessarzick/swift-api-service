d1d9a4fda44fc7bddc708b5c3a38d710bed8a8de
Colin Tessarzick <colintessarzick@hotmail.com>
2023-03-11 22:04:17 +0100

    refactor: create DNS records for App Runner custom domain

--- terraform/aws_route53.tf
+++ terraform/aws_route53.tf
@@ -20,15 +20,15 @@ resource "aws_route53_record" "public_domain" {
   zone_id         = data.aws_route53_zone.domain.zone_id
 }
 
-# resource "aws_route53_record" "api_service_domain_mapping" {
-#   depends_on = [aws_apprunner_custom_domain_association.api_service]
+resource "aws_route53_record" "api_service_domain_mapping" {
+  depends_on = [aws_apprunner_custom_domain_association.api_service]
 
-#   for_each = aws_apprunner_custom_domain_association.api_service.certificate_validation_records
+  for_each = aws_apprunner_custom_domain_association.api_service.certificate_validation_records
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.value]
-#   type            = each.value.type
-#   ttl             = 60
-#   zone_id         = data.aws_route53_zone.domain.zone_id
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.value]
+  type            = each.value.type
+  ttl             = 60
+  zone_id         = data.aws_route53_zone.domain.zone_id
+}
