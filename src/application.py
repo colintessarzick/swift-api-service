0c9e91d7a0c51005aa4a7635dd13d42cad1685d8
Colin Tessarzick <colintessarzick@hotmail.com>
2023-03-13 19:18:05 +0100

    chore: demo workflow

--- src/application.py
+++ src/application.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/awss')
+def aws():
+    return 'AWS User Group!'
+
 if __name__ == "__main__":
     app.run(debug=True)
